4fad36121e7b2f40e5a1fc9318ccdaef6ab26a05
Andrew Boza <andrew.m.boza@gmail.com>
2018-06-04 06:59:12 -0500

    Refactoring

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,24 @@
+﻿public enum EquipType
+{
+    Footwear,
+    Legwear,
+    Handwear,
+    Chestwear,
+    Waistwear,
+    Backwear,
+    Neckwear,
+    Headwear,
+    RightHanded,
+    LeftHanded
+}
+
+public enum AnimationType
+{
+    IdleUp, IdleLeft, IdleDown, IdleRight,
+    WalkUp, WalkLeft, WalkDown, WalkRight,
+    SlashUp, SlashLeft, SlashDown, SlashRight,
+    ThrustUp, ThrustLeft, ThrustDown, ThrustRight,
+    LooseUp, LooseLeft, LooseDown, LooseRight,
+    SpellcastUp, SpellcastLeft, SpellcastDown, SpellcastRight,
+    Fall
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,32 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEditor;
 using UnityEngine;
-
-public enum EquipType
-{
-    Footwear,
-    Legwear,
-    Handwear,
-    Chestwear,
-    Waistwear,
-    Backwear,
-    Neckwear,
-    Headwear,
-    RightHanded,
-    LeftHanded
-}
-
-public enum AnimationType
-{
-    IdleUp, IdleLeft, IdleDown, IdleRight,
-    WalkUp, WalkLeft, WalkDown, WalkRight,
-    SlashUp, SlashLeft, SlashDown, SlashRight,
-    ThrustUp, ThrustLeft, ThrustDown, ThrustRight,
-    LooseUp, LooseLeft, LooseDown, LooseRight,
-    SpellcastUp, SpellcastLeft, SpellcastDown, SpellcastRight,
-    Fall
-}
 
 public class Item
 {
@@ -68,14 +44,14 @@ public class Item
             spriteBinding.path = "";
             spriteBinding.propertyName = "m_Sprite";
 
-            int[] startAndRange = GetSpriteStartIndexAndRange(animationType);
-            ObjectReferenceKeyframe[] spriteKeyFrames = new ObjectReferenceKeyframe[startAndRange[1]];
+            var startAndRange = GetSpriteStartIndexAndRange(animationType);
+            ObjectReferenceKeyframe[] spriteKeyFrames = new ObjectReferenceKeyframe[startAndRange.Range];
             float timeValue = 0;
-            for (int i = 0; i < startAndRange[1]; i++)
+            for (int i = 0; i < startAndRange.Range; i++)
             {
                 spriteKeyFrames[i] = new ObjectReferenceKeyframe();
                 spriteKeyFrames[i].time = timeValue;
-                spriteKeyFrames[i].value = sprites[i + startAndRange[0]];
+                spriteKeyFrames[i].value = sprites[i + startAndRange.StartIndex];
                 timeValue += 1 / 8f;
             }
             AnimationUtility.SetObjectReferenceCurve(animClip, spriteBinding, spriteKeyFrames);
@@ -89,70 +65,81 @@ public class Item
         }
     }
 
-    private int[] GetSpriteStartIndexAndRange(AnimationType animationType)
+    private SpriteSheetAnimationInfo GetSpriteStartIndexAndRange(AnimationType animationType)
     {
         switch (animationType)
         {
             case AnimationType.IdleUp:
-                return new int[] { 60, 1 };
+                return new SpriteSheetAnimationInfo(60, 1);
             case AnimationType.IdleLeft:
-                return new int[] { 69, 1 };
+                return new SpriteSheetAnimationInfo(69, 1);
             case AnimationType.IdleDown:
-                return new int[] { 78, 1 };
+                return new SpriteSheetAnimationInfo(78, 1);
             case AnimationType.IdleRight:
-                return new int[] { 87, 1 };
+                return new SpriteSheetAnimationInfo(87, 1);
 
             case AnimationType.WalkUp:
-                return new int[] { 61, 8 };
+                return new SpriteSheetAnimationInfo(61, 8);
             case AnimationType.WalkLeft:
-                return new int[] { 70, 8 };
+                return new SpriteSheetAnimationInfo(70, 8);
             case AnimationType.WalkDown:
-                return new int[] { 79, 8 };
+                return new SpriteSheetAnimationInfo(79, 8);
             case AnimationType.WalkRight:
-                return new int[] { 88, 8 };
+                return new SpriteSheetAnimationInfo(88, 8);
 
             case AnimationType.SlashUp:
-                return new int[] { 96, 6 };
+                return new SpriteSheetAnimationInfo(96, 6);
             case AnimationType.SlashLeft:
-                return new int[] { 102, 6 };
+                return new SpriteSheetAnimationInfo(102, 6);
             case AnimationType.SlashDown:
-                return new int[] { 108, 6 };
+                return new SpriteSheetAnimationInfo(108, 6);
             case AnimationType.SlashRight:
-                return new int[] { 114, 6 };
+                return new SpriteSheetAnimationInfo(114, 6);
 
             case AnimationType.ThrustUp:
-                return new int[] { 28, 8 };
+                return new SpriteSheetAnimationInfo(28, 8);
             case AnimationType.ThrustLeft:
-                return new int[] { 36, 8 };
+                return new SpriteSheetAnimationInfo(36, 8);
             case AnimationType.ThrustDown:
-                return new int[] { 44, 8 };
+                return new SpriteSheetAnimationInfo(44, 8);
             case AnimationType.ThrustRight:
-                return new int[] { 52, 8 };
+                return new SpriteSheetAnimationInfo(52, 8);
 
             case AnimationType.LooseUp:
-                return new int[] { 120, 13 };
+                return new SpriteSheetAnimationInfo(120, 13);
             case AnimationType.LooseLeft:
-                return new int[] { 133, 13 };
+                return new SpriteSheetAnimationInfo(133, 13);
             case AnimationType.LooseDown:
-                return new int[] { 146, 13 };
+                return new SpriteSheetAnimationInfo(146, 13);
             case AnimationType.LooseRight:
-                return new int[] { 159, 13 };
+                return new SpriteSheetAnimationInfo(159, 13);
 
             case AnimationType.SpellcastUp:
-                return new int[] { 0, 7 };
+                return new SpriteSheetAnimationInfo(0, 7);
             case AnimationType.SpellcastLeft:
-                return new int[] { 7, 7 };
+                return new SpriteSheetAnimationInfo(7, 7);
             case AnimationType.SpellcastDown:
-                return new int[] { 14, 7 };
+                return new SpriteSheetAnimationInfo(14, 7);
             case AnimationType.SpellcastRight:
-                return new int[] { 21, 7 };
+                return new SpriteSheetAnimationInfo(21, 7);
 
             case AnimationType.Fall:
-                return new int[] { 172, 6 };
+                return new SpriteSheetAnimationInfo(172, 6);
 
             default:
-                Debug.LogWarning("Problem with Item.cs");
-                return new int[] { 0, 0 };
+                throw new InvalidEnumArgumentException();
         }
     }
+}
+
+public struct SpriteSheetAnimationInfo
+{
+    public SpriteSheetAnimationInfo(int startIndex, int range)
+    {
+        StartIndex = startIndex;
+        Range = range;
+    }
+
+    public int StartIndex { get; private set; }
+    public int Range { get; private set; }
 }
